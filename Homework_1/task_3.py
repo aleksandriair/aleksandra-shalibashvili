d7fe25589b44229d8d392a4c87a178f69ed3e2b2
Aleksandra Shalibashvili <aleksandriair@gmail.com>
2024-09-24 00:51:52 +0400

    Submitting Homework 1

--- Homework_1/task_3.py
+++ Homework_1/task_3.py
@@ -1,9 +1,9 @@
-a = int(input("გვერდი 1: "))
-b = int(input("გვერდი 2: "))
-c = int(input("გვერდი 3: "))
+side_a = int(input("გვერდი 1: "))
+side_b = int(input("გვერდი 2: "))
+side_c = int(input("გვერდი 3: "))
 
-peri = a + b + c
-s_peri = 1/2 * peri
-area = round((s_peri*(s_peri-a)*(s_peri-b)*(s_peri-c))**(1/2),2)
+perimeter = side_a + side_b + side_c
+semi_perimeter = 1/2 * perimeter
+area = round((semi_perimeter*(semi_perimeter-side_a)*(semi_perimeter-side_b)*(semi_perimeter-side_c))**(1/2),2)
 
-print("ფართობი =", area, ", პერიმეტრი =", peri)
+print("ფართობი =", area, ", პერიმეტრი =", perimeter)
